d92ed83f12c48843b04cb90aebbd3912f9d6b52e
Hanes Barbosa <hanes.barbosa@gmail.com>
2015-05-27 21:56:35 -0300

    Creating a little help note in case of missing arguments.

--- lib/google.rb
+++ lib/google.rb
@@ -5,29 +5,53 @@ require "google/version"
 module Google
 
   def self.search
-    browser = Mechanize.new
 
-    criteria = ARGV  
-    criteria = criteria.join(" ")
+    if ARGV.empty?
 
-    page = browser.get('http://google.com/')  
-    form = page.form_with(:name => 'f')
-    form.q = criteria
-    result = form.submit
+      show_help
 
-    entries = result.search("li.g")
+    else
 
-    puts ""
-    puts Paint["Results for: #{criteria}", "#006699"]
-    puts ""
+      browser = Mechanize.new
 
-    entries.each do |entry|
-      puts Paint[entry.search("h3.r").text, "#2ECCFA"]
-      puts Paint[entry.search("cite").text, "#A9E2F3"]
-      puts entry.search("span.st").text
-      puts Paint["http://google.com" + entry.search("h3.r").first.children.first.attributes["href"].value, "#045FB4"]
+      criteria = ARGV  
+      criteria = criteria.join(" ")
+
+      page = browser.get('http://google.com/')  
+      form = page.form_with(:name => 'f')
+      form.q = criteria
+      result = form.submit
+
+      entries = result.search("li.g")
+
       puts ""
+      puts Paint["Results for: #{criteria}", "#006699"]
+      puts ""
+
+      entries.each do |entry|
+        puts Paint[entry.search("h3.r").text, "#2ECCFA"]
+        puts Paint[entry.search("cite").text, "#A9E2F3"]
+        puts entry.search("span.st").text
+        puts Paint["http://google.com" + entry.search("h3.r").first.children.first.attributes["href"].value, "#045FB4"]
+        puts ""
+      end
+
     end
 
   end
+
+  private
+
+  def self.show_help
+    message = "\n"
+    message << "GOOGLE #{VERSION}\n"
+    message << "NAME\n"
+    message << "\tsearch, google\n"
+    message << "CRITERIA\n"
+    message << "\tKeywords used by the search utility to fetch results from Google’s engine.\n"
+    message << "SYNOPSIS\n"
+    message << "\tNAME CRITERIA\n\n"
+    puts message
+  end
+
 end
