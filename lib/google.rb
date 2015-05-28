2fc18e1508fffbf2fa06d7d3d0a35499501a29cf
Hanes Barbosa <hanes.barbosa@gmail.com>
2015-05-27 23:43:08 -0300

    Adding version option and number of results.

--- lib/google.rb
+++ lib/google.rb
@@ -5,53 +5,84 @@ require "google/version"
 module Google
 
   def self.search
-
-    if ARGV.empty?
-
-      show_help
-
-    else
-
-      browser = Mechanize.new
-
-      criteria = ARGV  
-      criteria = criteria.join(" ")
-
-      page = browser.get('http://google.com/')  
-      form = page.form_with(:name => 'f')
-      form.q = criteria
-      result = form.submit
-
-      entries = result.search("li.g")
-
-      puts ""
-      puts Paint["Results for: #{criteria}", "#006699"]
-      puts ""
-
-      entries.each do |entry|
-        puts Paint[entry.search("h3.r").text, "#2ECCFA"]
-        puts Paint[entry.search("cite").text, "#A9E2F3"]
-        puts entry.search("span.st").text
-        puts Paint["http://google.com" + entry.search("h3.r").first.children.first.attributes["href"].value, "#045FB4"]
-        puts ""
-      end
-
-    end
-
+    pick_menu
   end
 
   private
 
+  def self.fetch_results(criteria, count)
+    puts "Fetching Google results..."
+    browser = Mechanize.new
+
+    criteria = criteria.join(" ")
+
+    page = browser.get('http://google.com/')  
+    form = page.form_with(:name => 'f')
+    form.q = criteria
+    result = form.submit
+
+    entries = result.search("li.g")
+
+    puts ""
+    puts Paint["Results for: #{criteria}", "#006699"]
+    puts ""
+
+    entries.each do |entry|
+      break if count <= 0
+      puts Paint[entry.search("h3.r").text, "#2ECCFA"]
+      puts Paint[entry.search("cite").text, "#A9E2F3"]
+      puts entry.search("span.st").text
+      puts Paint["http://google.com" + entry.search("h3.r").first.children.first.attributes["href"].value, "#045FB4"]
+      puts ""
+      count -= 1
+    end
+  end
+
   def self.show_help
     message = "\n"
-    message << "GOOGLE #{VERSION}\n"
+    message << "GOOGLE #{VERSION}\n\n"
     message << "NAME\n"
     message << "\tsearch, google\n"
     message << "CRITERIA\n"
     message << "\tKeywords used by the search utility to fetch results from Google’s engine.\n"
     message << "SYNOPSIS\n"
-    message << "\tNAME CRITERIA\n\n"
+    message << "\tNAME [-v] CRITERIA\n"
+    message << "OPTIONS\n"
+    message << "\t-v\tShow version\n\n"
     puts message
+  end
+
+  def self.show_version
+    puts "GOOGLE V#{VERSION}"
+  end
+
+  def self.pick_menu
+    if ARGV.empty?
+      show_help
+    else
+      count = 10
+      options = ARGV.select { |i| i =~ /^-[a-z]{1}/ }
+
+      if options.include? '-v'
+        index = ARGV.index '-v'
+        ARGV.delete_at index
+        show_version
+      end
+
+      if options.include? '-c'
+        index = ARGV.index '-c'
+        begin
+          count = ARGV[index + 1]
+          count = count.to_i
+          ARGV.delete_at(index + 1)
+          ARGV.delete_at(index)
+        rescue Exception => e
+          puts "Wrong argument at -c: #{e.message}"
+        end
+      end
+
+      fetch_results(ARGV, count)
+    end
   end
 
 end
